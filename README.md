7c460778987fa48cc17fd2fbe5ff4b9a653d0b0e
Tommy Alex <iptux7@gmail.com>
2021-01-23 16:40:47 +0800

    init battery alert gnome extension
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

---
+++ README.md
@@ -0,0 +1,20 @@
+Battery Alert
+=============
+
+It's not the first time that I didn't turn on AC power,
+and my laptop turn off directly when battery drain to empty.
+
+So just notify me if I'm using battery or when battery is low,
+I will turn on AC power, or save my work and shutdown
+before battery is empty.
+
+Thanks [Dim On Battery Power][dim].
+
+[dim]: https://extensions.gnome.org/extension/947/dim-on-battery-power/
+
+License
+-------
+
+Battery Alert is licensed under [GPLv3][].
+
+[GPLv3]: http://www.gnu.org/copyleft/gpl.html
---
+++ battery-alert@iptux.github.io/extension.js
@@ -0,0 +1,85 @@
+/*
+Battery Alert - Get an alert when battery is low in gnome
+Copyright (C) 2021  Tommy Alex
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+'use strict';
+
+const Main = imports.ui.main;
+const UPower = imports.ui.status.power.UPower;
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+
+function BatteryAlert() {
+	this._init();
+}
+
+BatteryAlert.prototype = {
+	__proto__: function() {
+	},
+
+	_init: function() {
+		this.alertLevel = 25;
+		this._lastStatus = -1;
+		this._lastPercentage = -1;
+		this._uPowerProxy = Main.panel.statusArea["aggregateMenu"]._power._proxy;
+		this._uPowerSignal = this._uPowerProxy.connect('g-properties-changed', this._onPowerChange.bind(this));
+	},
+
+	isDischargeState: function(state) {
+		if ( state == UPower.DeviceState.DISCHARGING ) return true;
+		if ( state == UPower.DeviceState.PENDING_DISCHARGE ) return true;
+		return false;
+	},
+
+	_notify: function(msg) {
+		Main.notify(Extension.metadata.name, msg)
+	},
+
+	_onPowerChange: function() {
+		if ( this._uPowerProxy.IsPresent
+			&& this.isDischargeState(this._uPowerProxy.State) )
+		{
+			if ( this._uPowerProxy.Percentage <= this.alertLevel ) {
+				if ( this._lastPercentage < 0 || this._lastPercentage > this.alertLevel ) {
+					this._notify(`battery low: ${this._uPowerProxy.Percentage}`);
+				}
+			} else if ( ! this.isDischargeState(this._lastStatus) ) {
+				this._notify(`you are using battery`);
+			}
+		}
+
+		this._lastStatus = this._uPowerProxy.State;
+		this._lastPercentage = this._uPowerProxy.Percentage;
+	},
+
+	destroy: function() {
+		this._uPowerProxy.disconnect(this._uPowerSignal);
+	},
+}
+
+let batteryAlert = null;
+
+function init() {
+}
+
+function enable() {
+	batteryAlert = new BatteryAlert();
+}
+
+function disable() {
+	batteryAlert.destroy();
+	batteryAlert = null;
+}
